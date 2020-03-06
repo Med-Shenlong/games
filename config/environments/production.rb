829cbcc65f73f29b8c3de39136d848a72fbe6ec0
mohamed tounsi <medtounsi1987@gmail.com>
2020-03-06 19:19:20 +0100

    test prod4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,7 +117,7 @@ Rails.application.configure do
 
 
 #production heroku
-config.action_mailer.default_url_options = {:host => 'for-the-gamers.herokuapp.com', :protocol => 'http'}
+config.action_mailer.default_url_options = {:host => 'for-the-gamers.herokuapp.com', :protocol => 'https'}
 config.action_mailer.raise_delivery_errors = true
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
