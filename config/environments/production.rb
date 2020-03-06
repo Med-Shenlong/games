e2d6e55ea09ae5839ab14e63b1c605cfbc8a0678
mohamed tounsi <medtounsi1987@gmail.com>
2020-03-06 18:38:06 +0100

    test prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,15 +119,21 @@ Rails.application.configure do
 
 #production heroku
 
-config.action_mailer.default_url_options = {:host => 'for-the-gamers.herokuapp.com', :protocol => 'http'}
-config.action_mailer.smtp_settings = {
-   :address => "smtp.gmail.com",
-   :port => 587,
-   :domain => 'for-the-gamers.herokuapp.com',
-   :authentication => :plain,
-   :user_name => 'jaegereren08@gmail.com',
-   :password => 'juggernaut1987/8815@03'
- }
+config.action_mailer.default_url_options = {host: 'https://for-the-gamers.herokuapp.com/', :protocol => 'https'}
+
+  #Maildev
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'for-the-gamers.herokuapp.com'
+    authentication: => :plain
+    username: => 'jaegereren08@gmail.com',
+    password: => 'juggernaut1987/8815@03'
+
+  }
 
 
 
