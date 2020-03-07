8818a6d14e6e25e8d9c40275fb2fc166ccaeb240
mohamed tounsi <medtounsi1987@gmail.com>
2020-03-07 15:39:15 +0100

    send transactional email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,10 +1,4 @@
-class TestMailer < ActionMailer::Base
-  def hello
-    mail(
-      :subject => 'Hello from Postmark',
-      :to  => 'shenlong@tekken.fr',
-      :from => 'shenlong@tekken.fr',
-      :html_body => '<strong>Hello</strong> dear Postmark user.',
-      :track_opens => 'true')
-  end
+class ApplicationMailer < ActionMailer::Base
+  default from: 'mohamedtounsi88@hotmail.com'
+  layout 'mailer'
 end
