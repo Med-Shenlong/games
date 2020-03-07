c402c82001adf217fe9aa30c4002dfac0db92e80
mohamed tounsi <medtounsi1987@gmail.com>
2020-03-07 16:16:45 +0100

    test prod 6

--- Gemfile
+++ Gemfile
@@ -72,4 +72,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 #postmark server
 
-gem 'postmark-rails'
+
--- config/application.rb
+++ config/application.rb
@@ -14,8 +14,7 @@ module Games
     config.site = {
      name: 'Games'
 
-     config.action_mailer.delivery_method = :postmark
-     config.action_mailer.postmark_settings = { :api_token => Rails.application.secrets.postmark_api_token }
+
 
     }
 
