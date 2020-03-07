4fadecaa775f9b4e158cb60ddfd3af5025dc55b7
mohamed tounsi <medtounsi1987@gmail.com>
2020-03-07 16:37:54 +0100

    prod8

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,20 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+config.action_mailer.default_url_options = {:host => 'for-the-gamers.herokuapp.com', :protocol => 'https'}
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.smtp_settings = {
+   :address => "smtp.gmail.com",
+   :port => 587,
+   :domain => 'for-the-gamers.herokuapp.com',
+   :authentication => :plain,
+   :enable_starttls_auto => true,
+   :user_name => 'jaegereren08@gmail.com',
+   :password => 'juggernaut1987/8815@03'
+ }
+
 end
 
   # Inserts middleware to perform automatic connection switching.
@@ -119,19 +133,7 @@ end
 
 
 #production heroku
-config.action_mailer.default_url_options = {:host => 'for-the-gamers.herokuapp.com', :protocol => 'https'}
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.perform_deliveries = true
-config.action_mailer.smtp_settings = {
-   :address => "smtp.gmail.com",
-   :port => 587,
-   :domain => 'for-the-gamers.herokuapp.com',
-   :authentication => :plain,
-   :enable_starttls_auto => true,
-   :user_name => 'jaegereren08@gmail.com',
-   :password => 'juggernaut1987/8815@03'
- }
+
 
 
 
