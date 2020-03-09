591b11b9ebdd4db91fbe29434e67101804775a1a
mohamed tounsi <medtounsi1987@gmail.com>
2020-03-09 19:36:00 +0100

    smtp config2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,18 +95,17 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 config.action_mailer.default_url_options = {:host => 'for-the-gamers.herokuapp.com', :protocol => 'http'}
-
+config.action_mailer.raise_delivery_errors = true
 config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-
-    address: 'mail.smtp2go.com',
-    port: 2525,
-    domain: 'for-the-gamers.herokuapp.com',
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: "jaergereren08@gmail.com",
-    password: "root"
-  }
+config.action_mailer.smtp_settings = {
+:address => "smtp.gmail.com",
+:port => "587",
+:domain => "gmail.com",
+:user_name => "jaegereren08@gmail.com",
+:password => "root",
+:authentication => "plain",
+:enable_starttls_auto => true
+}
 
 
 
