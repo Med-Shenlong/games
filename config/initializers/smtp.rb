1bcd501c81308fa239c5f1d61a518b5e3f36ddaa
mohamed tounsi <medtounsi1987@gmail.com>
2020-03-10 20:08:00 +0100

    prod15

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,15 +1,9 @@
 ActionMailer::Base.smtp_settings = {
   address: 'smtp.sendgrid.net',
   port: 587,
-<<<<<<< HEAD
-  domain: 'for-the-gamers.herokuapp.com',
-  user_name: ENV['lucifer87'],
-  password: ENV['bigboss1988'],
-=======
-  domain: 'http://for-the-gamers.herokuapp.com/',
-  user_name: ENV['******'],
-  password: ENV['******'],
->>>>>>> 0f294256dc408575d55de7b454478abe04e01382
+  domain: 'for-the-gamers.herokuapp.com/',
+  user_name: ENV['SENDGRID_lucifer87'],
+  password: ENV['SENDGRID_bigboss1988'],
   authentication: :login,
   enable_starttls_auto: true
 }
