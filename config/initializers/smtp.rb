cce4c5292459d0bb9e301a9b81b7d3e8ef244160
mohamed tounsi <medtounsi1987@gmail.com>
2020-03-10 21:24:08 +0100

    always push

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   address: 'smtp.sendgrid.net',
   port: 587,
   domain: 'for-the-gamers.herokuapp.com/',
-  user_name: ENV['SENDGRID_username'],
-  password: ENV['SENDGRID_password'],
+  user_name: ENV['lucifer87'],
+  password: ENV['bigboss1988'],
   authentication: :login,
   enable_starttls_auto: true
 }
