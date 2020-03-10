0f294256dc408575d55de7b454478abe04e01382
Mohamed Tounsi <55838781+Med-Shenlong@users.noreply.github.com>
2020-03-10 19:46:10 +0100

    Update smtp.rb

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   address: 'smtp.sendgrid.net',
   port: 587,
   domain: 'http://for-the-gamers.herokuapp.com/',
-  user_name: ENV['lucifer87'],
-  password: ENV['bigboss1988'],
+  user_name: ENV['******'],
+  password: ENV['******'],
   authentication: :login,
   enable_starttls_auto: true
 }
