2c7b2df5b768b32bc6b65a8bff9c42ed7fd64483
mohamed tounsi <medtounsi1987@gmail.com>
2020-03-12 22:10:16 +0100

    setup heroku production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,17 +95,17 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-config.action_mailer.default_url_options = { :host => ENV['https://for-the-gamers.herokuapp.com/'] }
+config.action_mailer.default_url_options = { :host => 'for-the-gamers.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
-    :port => '587',
-    :authentication => :plain,
-    :user_name => ENV[''],
-    :password => ENV[''],
-    :domain => 'heroku.com',
-    :enable_starttls_auto => true
+
+  address: 'smtp.gmail.com',
+  port: 587,
+  domain: 'gmail.com',
+  authentication: :plain,
+  user_name: ENV['GMAIL_USERNAME'],
+  password: ENV['GMAIL_PASSWORD'],
   }
 
 
