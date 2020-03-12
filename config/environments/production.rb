506ec3072c3b0dcc165330eafddaa275e550dbf3
mohamed tounsi <medtounsi1987@gmail.com>
2020-03-12 22:23:31 +0100

    prod test 18

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
 
   config.serve_static_assets = true
 
-
+config.consider_all_requests_local = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
