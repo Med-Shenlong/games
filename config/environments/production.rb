63454c683faa61b09b491ade5273277b9cea7f9b
mohamed tounsi <medtounsi1987@gmail.com>
2020-03-13 14:55:55 +0100

    smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,20 +98,19 @@ config.logger.level = Logger::DEBUG
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-# config.action_mailer.default_url_options = { :host => 'https://for-the-gamers.herokuapp.com' }
+config.action_mailer.default_url_options = { :host => 'https://for-the-gamers.herokuapp.com' }
 
 
-# config.action_mailer.delivery_method = :smtp
-# config.action_mailer.perform_deliveries = true
-# config.action_mailer.raise_delivery_errors = true
-# config.action_mailer.smtp_settings = {
-#   address: 'smtp.gmail.com',
-#   port: 587,
-#   authentication: :plain,
-#   user_name: ENV['SMTP_USERNAME'],
-#   password: ENV['SMTP_PASSWORD'],
-#   enable_starttls_auto: true
-# }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  authentication: => :plain,
+  user_name: => "GMAIL_ID",
+  password: => "GMAIL_PASSWORD",
+  enable_starttls_auto: true
+}
+
 
 
 end
