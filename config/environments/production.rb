47e8cc190c8fdb96470d54dddc33b27a5557e91e
mohamed tounsi <medtounsi1987@gmail.com>
2020-03-13 14:26:06 +0100

    prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,15 +103,16 @@ config.action_mailer.default_url_options = { :host => 'https://for-the-gamers.he
 
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
 config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
+  address: 'smtp.gmail.com',
   port: 587,
-  authentication: "plain",
-  user_name: ENV["SMTP_USERNAME"],
+  authentication: :plain,
+  user_name: ENV['SMTP_USERNAME'],
   password: ENV['SMTP_PASSWORD'],
   enable_starttls_auto: true
 }
-config.action_mailer.raise_delivery_errors = false
+
 
 end
 
