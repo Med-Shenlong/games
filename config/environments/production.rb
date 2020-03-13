0f9bc73ecfd5f70418ba342e80107d51a63ffd78
mohamed tounsi <medtounsi1987@gmail.com>
2020-03-13 14:14:30 +0100

    edit production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,6 +102,7 @@ config.action_mailer.default_url_options = { :host => 'https://for-the-gamers.he
 
 
 config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
 config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
