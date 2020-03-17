9023eb0120afc91bc21115b229fccee962715b18
mohamed tounsi <medtounsi1987@gmail.com>
2020-03-17 12:37:25 +0100

    setup gmail account

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,19 +100,25 @@ config.logger.level = Logger::DEBUG
 
 
 
-config.action_mailer.default_url_options = { host: 'https://for-the-gamers.herokuapp.com', protocol: 'http' }
+config.action_mailer.default_url_options = { host: 'https://for-the-gamers.herokuapp.com' }
 
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = false
+config.action_mailer.default :charset => "utf-8"
+
+
+
    config.action_mailer.smtp_settings = {
-     address: 'smtp.mail.com',
-     port: 587,
-     domain: 'https://for-the-gamers.herokuapp.com',
-     user_name: ENV['GMAIL_USERNAME'],
-     password: ENV['GMAIL_PASSWORD'],
-     authentication: :plain,
-     enable_starttls_auto: true }
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "https://for-the-gamers.herokuapp.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
+}
 
 
 
