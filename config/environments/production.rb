fbdb88c2306dfe02bcba7b19e3778653b114d92c
mohamed tounsi <medtounsi1987@gmail.com>
2020-03-17 13:24:00 +0100

    edit gmail smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,9 +111,9 @@ config.action_mailer.default :charset => "utf-8"
 
 
    config.action_mailer.smtp_settings = {
-  address: "gmail.com",
+  address: "smtp.gmail.com",
   port: 587,
-  domain: "https://for-the-gamers.herokuapp.com",
+  domain: "gmail.com",
   authentication: :plain,
   enable_starttls_auto: true,
   user_name: ENV["GMAIL_USERNAME"],
